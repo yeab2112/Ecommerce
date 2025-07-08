0ea427056e237bf21fb97eed153a29a7f8910298
Yeabsira Aychiluhim <yeabsiraaychiluhim2112@gmail.com>
2025-07-08 16:42:12 +0300

    add the review page

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -84,7 +84,7 @@ const getUserOrders = async (req, res) => {
       .select('_id orderNumber items total createdAt status paymentMethod isPaid deliveryInfo')
       .populate({
         path: 'items.product',
-        select: 'name price images slug' // Include essential product details
+        select: '_id name price images slug' // Include essential product details
       })
       .lean();
 
