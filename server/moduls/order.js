9a896959b720f37d9af901f9c529ab2f1fd7b7f5
Yeabsira Aychiluhim <yeabsiraaychiluhim2112@gmail.com>
2025-07-10 12:43:08 +0300

    change the status when the payment is cash

--- server/moduls/order.js
+++ server/moduls/order.js
@@ -79,7 +79,7 @@ const orderSchema = new mongoose.Schema({
   total: { type: Number, required: true },
   status: {
     type: String,
-    enum: ['pending', 'processing', 'shipped', 'delivered', 'cancelled'],
+    enum: ['pending', 'processing', 'shipped', 'delivered', 'cancelled','received'],
     default: 'pending',
     index: true  // Index for faster status queries
   },
